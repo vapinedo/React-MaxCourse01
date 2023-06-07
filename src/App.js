3bb1cac1800ceb8c65b312d6d0d9cc501b20268e
Victor Pinedo <victorpinedomartinez@gmail.com>
2023-06-06 20:47:43 -0500

    41. A Closer Look At JSX

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react"; 
 import Expenses from "./components/Expenses";
 
 function App() {
@@ -23,6 +24,13 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+
+  // return React.createElement(
+  //   "div", 
+  //   {},  
+  //   React.createElement("h2", {}, "Lets get started!"), 
+  //   React.createElement(Expenses, {items: expenses}) 
+  // );
 
   return (
     <div>
