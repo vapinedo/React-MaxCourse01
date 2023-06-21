56e42c708fb468f2a6f6caa1493ff98e0735b859
Victor Pinedo <victorpinedomartinez@gmail.com>
2023-06-21 16:52:07 -0500

    87. Using Stateful Lists

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,11 +1,21 @@
 import "./Expenses.css" 
-import React from "react"; 
-import Card from "../UI/Card";
+   import Card from "../UI/Card";
+import React, { useState } from "react"; 
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from './ExpensesFilter';
 
 const Expenses = (props) => {
+
+    const [filteredYear, setFilteredYear] = useState('2020');
+
+    const filterChangeHandler = selectedYear => {
+      setFilteredYear(selectedYear);
+    };
+
     return (
         <Card className="expenses">
+            <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+
             {props.expenses.map(expense => (
                 <ExpenseItem
                     key={expense.id} 
